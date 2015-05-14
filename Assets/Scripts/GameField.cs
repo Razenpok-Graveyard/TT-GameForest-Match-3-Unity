dd4e1ecd44f4969fb23dca14c294f88d5c35751c
Dia6lo <Dia6lo@mail.ru>
2015-05-14 19:37:32 +1100

    Add tile spawning and killing

--- Assets/Scripts/GameField.cs
+++ Assets/Scripts/GameField.cs
@@ -1,42 +1,71 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Xml.Linq;
+using UnityEngine;
 
 public class GameField : MonoBehaviour
 {
-	private const int Width = 8;
-	private const int Height = 8;
-	public GameObject BackgroundPrefab;
-	public GameObject[] TilePrefabs;
-	private readonly GameObject[,] backgroundTiles = new GameObject[Width, Height];
-	private Tile[,] field = new Tile[Width,Height];
+    private const int Width = 8;
+    private const int Height = 8;
+    private static float textureWidth;
+    private static float textureHeight;
+    private readonly Transform[,] backgroundTiles = new Transform[Width, Height];
+    public GameObject BackgroundPrefab;
+    private Tile[,] tiles = new Tile[Width, Height];
+    public GameObject[] TilePrefabs;
 
-	// Use this for initialization
-	void Start ()
-	{
-		SpawnBackgroundTiles();
-	}
+    private void Start()
+    {
+        var spriteRenderer = BackgroundPrefab.GetComponent<SpriteRenderer>();
+        var texture = spriteRenderer.sprite.texture;
+        // Adjust to Unity units
+        textureWidth = texture.width/100f;
+        textureHeight = texture.height/100f;
+        SpawnBackgroundTiles();
+        FillField();
+    }
 
-	private void SpawnBackgroundTiles()
-	{
-		var spriteRenderer = BackgroundPrefab.GetComponent<SpriteRenderer>();
-		if (!spriteRenderer)
-		{
-			Debug.Log("There is no Sprite Renderer in Background");
-			return;
-		}
-		var texture = spriteRenderer.sprite.texture;
-		// Adjust to Unity units
-		var textureWidth = texture.width / 100f;
-		var textureHeight = texture.height / 100f;
-		for (var x = 0; x < Width; x++)
-			for (var y = 0; y < Height; y++)
-			{
-				var position = new Vector3(x * textureWidth, y * textureHeight);
-				backgroundTiles[x,y] = (GameObject) Instantiate(BackgroundPrefab, position, Quaternion.identity);
-			}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+    private void SpawnBackgroundTiles()
+    {
+        for (var x = 0; x < Width; x++)
+            for (var y = 0; y < Height; y++)
+            {
+                var position = new Vector3(x*textureWidth, y*textureHeight);
+                var backgroundTile = (GameObject) Instantiate(BackgroundPrefab, position, Quaternion.identity);
+                backgroundTile.transform.parent = transform;
+                backgroundTiles[x, y] = backgroundTile.transform;
+            }
+    }
+
+    private void FillField()
+    {
+        for (var x = 0; x < Width; x++)
+            for (var y = 0; y < Height; y++)
+            {
+                SpawnTile(x);
+            }
+    }
+
+    private void SpawnTile(int column)
+    {
+        var position = new Vector3(column*textureWidth, Height*textureHeight);
+        var tilePrefab = TilePrefabs[Random.Range(0, TilePrefabs.Length - 1)];
+        var tile = (GameObject) Instantiate(tilePrefab, position, Quaternion.identity);
+        for (var y = 0; y < Height; y++)
+        {
+            if (tiles[column, y] != null) continue;
+            var tileComponent = tile.GetComponent<Tile>();
+            tiles[column, y] = tileComponent;
+            tileComponent.MoveToPoint(backgroundTiles[column, y].position);
+            break;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!tiles[0, 0].IsMoving)
+        {
+            tiles[0,0].Kill();
+        }
+        Debug.Log(tiles[0,0]);
+    }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tile : MonoBehaviour {
+public class Tile : MonoBehaviour
+{
+    private const int Speed = 8;
+    private const float ShrinkScale = 0.1f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public bool IsMoving { get; private set; }
+
+    public void MoveToPoint(Vector3 destination)
+    {
+        if (IsMoving) return;
+        IsMoving = true;
+        StartCoroutine(MoveTo(destination));
+    }
+
+    public void OnMouseDown()
+    {
+        Kill();
+    }
+
+    public void Kill()
+    {
+        IsMoving = true;
+        StartCoroutine(Shrink());
+    }
+
+    private IEnumerator Shrink()
+    {
+        while (transform.localScale.x > ShrinkScale)
+        {
+            transform.localScale -= Vector3.one*Time.deltaTime;
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+
+    private IEnumerator MoveTo(Vector3 destination)
+    {
+        while (destination != transform.position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, Speed * Time.deltaTime);
+            yield return null;
+        }
+        IsMoving = false;
+    }
 }
